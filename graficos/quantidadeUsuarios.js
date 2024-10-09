57a68311fd2c9ff8cceb4446f2168fedc7ac3cd4
dlnpires22 <dlnpires22@gmail.com>
2024-10-09 08:08:35 -0300

    redes ds

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,13 +4,30 @@ async function quantidadeUsuarios() {
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
-  
+    
+    const data = [
+      {
+        x: nomeDasRedes,
+        y: quantidadeUsuarios,
+        type: 'bar'
+      }
+      
+    ]
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+
+    const data = [
+      {
+        x: 'nomeDasRedes',
+        y: quantidadeDeUsuarios,
+        type: 'bar',
+        marker: {
+          color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+        }
+      }
+    ]
+    
   }
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: 'bar'
-    }
-  ]
   quantidadeUsuarios()
